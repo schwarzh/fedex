854c1abd0040bc84aaaee890102a7e7f388cbdf9
Jon Buda <jon.buda@gmail.com>
2016-02-05 16:26:23 -0600

    Basic Support for Freight Rates

---
+++ lib/fedex/request/freight_rate.rb
@@ -0,0 +1,53 @@
+require 'fedex/request/base'
+
+module Fedex
+  module Request
+    class FreightRate < Rate
+
+      private
+
+      # Add information for shipments
+      def add_requested_shipment(xml)
+        xml.RequestedShipment{
+          xml.ServiceType service_type if service_type
+          add_shipper(xml)
+          add_recipient(xml)
+          xml.ShippingChargesPayment{
+            xml.PaymentType 'SENDER'
+            xml.Payor{
+              xml.ResponsibleParty{
+                xml.AccountNumber @freight_account[:account_number]
+              }
+            }
+          }
+          add_freight_shipment_detail(xml)
+        }
+      end
+
+      def add_freight_shipment_detail(xml)
+        xml.FreightShipmentDetail{
+          xml.AlternateBilling{
+            xml.AccountNumber @freight_account[:account_number]
+            xml.Address{
+              xml.StreetLines @freight_account[:address]
+              xml.City @freight_account[:city]
+              xml.StateOrProvinceCode @freight_account[:state]
+              xml.PostalCode @freight_account[:postal_code]
+              xml.CountryCode @freight_account[:country_code]
+            }
+          }
+          xml.Role 'SHIPPER'
+          xml.LineItems{
+            xml.FreightClass 'CLASS_050'
+            xml.Packaging 'PALLET'
+            xml.Weight{
+              xml.Units @packages.first[:weight][:units]
+              xml.Value @packages.first[:weight][:value]
+            }
+          }
+        }
+      end
+
+    end
+  end
+end

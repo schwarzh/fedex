2898a5e2c3190ecbae11e73c6a8ae2e7b4ec8c30
Andrew Briggs <theandrewbriggs@gmail.com>
2017-04-21 15:45:56 -0500

    Add hardcoded commercial invoice function to document

--- lib/fedex/request/document.rb
+++ lib/fedex/request/document.rb
@@ -14,8 +14,11 @@ module Fedex
 
       def add_custom_components(xml)
         super
-
-        add_shipping_document(xml) if @shipping_document
+        if @shipping_document == "COMMERCIAL_INVOICE"
+          add_commercial_invoice(xml)
+        else
+          add_shipping_document(xml) if @shipping_document
+        end
       end
 
       private
@@ -27,6 +30,23 @@ module Fedex
             xml.ShippingDocumentTypes type
           end
           hash_to_xml(xml, @shipping_document.reject{ |k| k == :shipping_document_types})
+        }
+      end
+
+      # If additional types of documents are needed, individual functions
+      # can go here. Basically, these are hardcoded required values, and
+      # shouldn't be in Blackbox core code just because the fedex gem and
+      # API are quirky and bad
+      #  - ajb, 21 Apr 2017
+      def add_commercial_invoice(xml)
+        xml.ShippingDocumentSpecification{
+          xml.ShippingDocumentTypes "COMMERCIAL_INVOICE"
+          xml.CommercialInvoiceDetail {
+            xml.Format {
+              xml.ImageType "PDF"
+              xml.StockType "PAPER_LETTER"
+            }
+          }
         }
       end
 

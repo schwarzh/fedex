93c3044aef23042fc54787497e4949af9f9c4005
schwarzh <schwarzh@cs.wisc.edu>
2019-01-30 19:30:25 -0600

    Add delivery_timestamp to initializer

--- lib/fedex/rate.rb
+++ lib/fedex/rate.rb
@@ -18,9 +18,10 @@ module Fedex
     #     @total_net_freight #The freight charge minus dicounts
     #     @total_surcharges #The total amount of all surcharges applied to this shipment
     #     @total_base_charge #The total base charge
-    attr_accessor :service_type, :transit_time, :rate_type, :rate_zone, :total_billing_weight, :total_freight_discounts, :total_net_charge, :total_taxes, :total_net_freight, :total_surcharges, :total_base_charge, :total_duties_and_taxes
+    attr_accessor :service_type, :delivery_timestamp, :transit_time, :rate_type, :rate_zone, :total_billing_weight, :total_freight_discounts, :total_net_charge, :total_taxes, :total_net_freight, :total_surcharges, :total_base_charge, :total_duties_and_taxes
     def initialize(options = {})
       @service_type = options[:service_type]
+      @delivery_timestamp = options[:delivery_timestamp]
       @transit_time = options[:transit_time]
       @rate_type = options[:rate_type]
       @rate_zone = options[:rate_zone]
